e385040619b7d6064ebc03c2fd0f5628a5094ca7
Gmongtor <gmongtor@myuax.com>
2024-05-08 10:12:54 +0200

    2

--- Ejercicio5/src/main/java/Twitter/Utils.java
+++ Ejercicio5/src/main/java/Twitter/Utils.java
@@ -1,2 +1,13 @@
+package Twitter;
+
+public class Utils {
+    public static boolean isValidEmail(String email) {
+        return email != null && email.contains("@");
+    }
+
+    public static boolean isValidAlias(String alias) {
+        return alias != null && alias.matches("[a-zA-Z0-9]+");
+    }
+}
 
 
