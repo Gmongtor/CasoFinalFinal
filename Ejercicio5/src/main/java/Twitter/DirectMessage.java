121236bc31b0c5e8f69c3173f2777620eb4a4b25
Gmongtor <gmongtor@myuax.com>
2024-05-08 10:35:16 +0200

    7

--- Ejercicio5/src/main/java/Twitter/DirectMessage.java
+++ Ejercicio5/src/main/java/Twitter/DirectMessage.java
@@ -1,4 +1,19 @@
 package Twitter;
+public class DirectMessage extends Tweet {
+    private UserAccount receiver;
 
-public class DirectMessage {
+    public DirectMessage(UserAccount sender, UserAccount receiver, String message) {
+        super(sender, message);
+        this.receiver = receiver;
+    }
+
+    public UserAccount getReceiver() {
+        return receiver;
+    }
+
+    @Override
+    public String toString() {
+        return "Direct message from " + sender.getAlias() + " to " + receiver.getAlias() + " at " + time + ": " + message;
+    }
 }
+
